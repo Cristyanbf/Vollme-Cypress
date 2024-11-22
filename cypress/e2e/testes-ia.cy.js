461ad733b6e0677f1bde390a360e38ecb128619f
Cristyanbf <cristyanplsc@hotmail.com>
2024-11-21 22:14:47 -0300

    novos testes

--- cypress/e2e/testes-ia.cy.js
+++ cypress/e2e/testes-ia.cy.js
@@ -17,22 +17,4 @@ describe('Teste de requisição da API para listar clínicas', () => {
     });
 });
 
-describe('Teste de logout no frontend', () => {
-    it('Deve desautenticar o usuário após o logout', () => {
-        cy.login(Cypress.env('email'), Cypress.env('senha'))
-        // Visita a página onde está o botão de logout
-        cy.visit('/dashboard');
-
-        // Executa a ação de logout, como clicar em um botão
-        cy.get('.sc-fLcnxK').click(); // Substitua com o seletor do botão de logout real
-
-        // Verifica se o usuário foi desautenticado
-        cy.window().its('localStorage.authToken').should('not.exist'); // Verifica se o token de autenticação foi removido do localStorage
-
-        // Verifica se o usuário foi redirecionado para a página de login (opcional)
-        cy.url().should('include', '/'); // Verifica se a URL contém a página de login
-
-        // Adicione mais verificações conforme necessário, como a exibição de mensagens de sucesso, etc.
-    });
-});
 
